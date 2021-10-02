98ca22bde038be94bfc35afd0ead9e8842fc9bbf
Msambi-ops <msambiroman@gmail.com>
2021-10-02 19:19:37 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Test Page</title>
+</head>
+<style>
+    .test h1 {
+        color: red;
+    }
+</style>
+
+<body>
+    <div class="test">
+        <h1>Test H1</h1>
+    </div>
+</body>
+
+</html>
